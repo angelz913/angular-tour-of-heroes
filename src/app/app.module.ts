7b97e5fe63298ed52cc58855bf4cb8d38d068828
Angel Zhang <anzhouzhang913@gmail.com>
2021-09-21 22:19:33 -0400

    Add hero name editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
+// @NgModule decorator annotates the top-level AppModule class
 @NgModule({
+  // contains a list of declared modules that the application uses
   declarations: [
     AppComponent,
     HeroesComponent
   ],
+  // contains a list of external modules that the application needs
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
