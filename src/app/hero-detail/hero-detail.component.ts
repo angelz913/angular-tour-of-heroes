7a99d904ebc5fd9613ec350ddfaaaf8f2bbf896c
Angel Zhang <anzhouzhang913@gmail.com>
2021-09-22 23:14:41 -0400

    Change HeroesComponent to use HeroService

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -2,16 +2,16 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
 
 @Component({
-  selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+    selector: 'app-hero-detail',
+    templateUrl: './hero-detail.component.html',
+    styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input() hero?: Hero;
-  constructor() { }
+    @Input() hero?: Hero;
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
