23dd076f2d94a99b2f9513e0f4372916586a767b
Angel Zhang <anzhouzhang913@gmail.com>
2021-09-23 08:20:53 -0400

    Add AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // path: a string that matches the URL in the browser address bar
+  // component: the component created when navigating to this route
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
