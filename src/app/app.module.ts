d7bb13eaf8f082642dd3fe5fdaf877dd522c40ae
Angel Zhang <anzhouzhang913@gmail.com>
2021-09-22 22:15:40 -0400

    Create a separate component for hero's details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 // @NgModule decorator annotates the top-level AppModule class
 @NgModule({
   // contains a list of declared modules that the application uses
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   // contains a list of external modules that the application needs
   imports: [
