2a86807eda0119bb854184e56896a389e0d57226
Angel Zhang <anzhouzhang913@gmail.com>
2021-09-24 00:46:45 -0400

    Add HTTP & Error Handling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,23 +12,31 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+
 // @NgModule decorator annotates the top-level AppModule class
 @NgModule({
-  // contains a list of declared modules that the application uses
-  declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessagesComponent,
-    DashboardComponent
-  ],
-  // contains a list of external modules that the application needs
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    // contains a list of declared modules that the application uses
+    declarations: [
+        AppComponent,
+        HeroesComponent,
+        HeroDetailComponent,
+        MessagesComponent,
+        DashboardComponent
+    ],
+    // contains a list of external modules that the application needs
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule,
+        // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+        // and returns simulated server responses.
+        // Remove it when a real server is ready to receive requests.
+        HttpClientInMemoryWebApiModule.forRoot(
+            InMemoryDataService, { dataEncapsulation: false }
+        )
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,14 +3,21 @@ import { Hero } from './hero';
 import { Heroes } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 export class HeroService {
 
+    private heroesUrl = 'api/heroes';  // URL to web api
+
     // service in service
-    constructor(private messageService: MessageService) { }
+    constructor(
+        private httpClient: HttpClient,
+        private messageService: MessageService
+    ) { }
 
     getHero(id: number): Observable<Hero> {
         // ! means you're certain that the object is not undefined
@@ -20,9 +27,40 @@ export class HeroService {
     }
 
     getHeroes(): Observable<Hero[]> {
-        const heroes = of(Heroes);
-        this.messageService.add('HeroService: fetched heroes');
-        return heroes;
+        // const heroes = of(Heroes);
+        // this.messageService.add('HeroService: fetched heroes');
+        // return heroes;
+        return this.httpClient.get<Hero[]>(this.heroesUrl)
+            .pipe(
+                tap(_ => this.log('fetched heroes')),
+                catchError(this.handleError<Hero[]>('getHeroes', []))
+            );
     }
     
+    // Log a HeroService message with the MessageService
+    private log(message: string) {
+        this.messageService.add(`HeroService: ${message}`);
+    }
+
+    /**
+     * Handle Http operation that failed.
+     * Let the app continue.
+     * @param operation - name of the operation that failed
+     * @param result - optional value to return as the observable result
+     */
+    private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+        // TODO: send the error to remote logging infrastructure
+        console.error(error); // log to console instead
+
+        // TODO: better job of transforming error for user consumption
+        this.log(`${operation} failed: ${error.message}`);
+
+        // Let the app keep running by returning an empty result.
+        return of(result as T);
+    };
+}
+    
+
 }
